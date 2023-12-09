053e88bfd0d52599cb3e3a949dbc08e2edd95e72
Jordan147 <31441851+Jordan147@users.noreply.github.com>
2023-12-09 21:08:30 +0800

    1. Add env-yocto.sh to setup build environment for bitbake package build 2. Add working-diary.sty for working progress recording

---
+++ env-yocto.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This script will run into container
+
+# source the common variables
+
+. imx-6.1.36-2.1.0/env.sh
+
+#
+
+if [ ! -d "$YOCTO_DIR" ]; then
+    echo "Yocto project folder not existing !!! Create it."
+    mkdir -p ${YOCTO_DIR}
+else
+    echo "Yocto folder ready !"
+fi
+cd ${YOCTO_DIR}
+
+# Init
+
+if [ ! -d .repo ]; then
+    echo "Start to setup repo"
+    repo init \
+        -u ${REMOTE} \
+        -b ${BRANCH} \
+        -m ${MANIFEST}
+
+    repo sync -j`nproc`
+else
+    echo "Found existing repo"
+fi
+
+# source the yocto env
+
+echo -e "MACHINE=${MACHINE}, DISTRO=${DISTRO}, IMAGES=${IMAGES}"
+EULA=1 MACHINE="${MACHINE}" DISTRO="${DISTRO}" source imx-setup-release.sh -b build_${DISTRO}
